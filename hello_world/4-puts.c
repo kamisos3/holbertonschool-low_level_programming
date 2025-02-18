7e0d0d080f1a78adefde40c347c39c31ed8d6a69
kamisos3 <kamilasostre11@gmail.com>
2025-02-18 12:36:01 -0400

    Creating a C program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main (void)
 {
-	printf("Programming is like building a multilingual puzzle\n") ;
+	char message[] = "/"Programming is like building a multilingual puzzle";
+
+	puts("Programming is like building a multilingual puzzle\n") ;
+	
 	return(0) ;
+
 }

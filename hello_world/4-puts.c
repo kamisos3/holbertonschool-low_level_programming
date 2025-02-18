ade8b95400b1a6e456eb35e853190606b28e7251
kamisos3 <kamilasostre11@gmail.com>
2025-02-18 13:57:45 -0400

    Creating a C program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 /** 
  * main - Using puts to write a C language program
+ *
+ *
  */
 
 #include <stdio.h>
-
-init main (void)
+init main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

68d5afbc4dcb4abe7f304d9c1d6e1a1063d7d52d
kamisos3 <kamilasostre11@gmail.com>
2025-02-18 14:49:41 -0400

    Using printf command to write in C program

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main-Using printf to write program in C
- *
- * Return
- * Always 0
- *
- */
+* main-Using printf to write program in C
+*
+* Return
+* Always 0
+*
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

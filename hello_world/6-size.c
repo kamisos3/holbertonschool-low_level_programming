6290d912f02a1f3ec1050651ae40ab36551d3f25
kamisos3 <kamilasostre11@gmail.com>
2025-02-19 11:56:19 -0400

    Getting various types of sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-printf("Size of char =  %u bytes\n", sizeof(char));
-printf("Size of int =  %u bytes\n", sizeof(int));
-printf("Size of long int = %u bytes\n", sizeof(long int));
-printf("Size of lon long int = %u bytes\n", sizeof(long long int));
-printf("Size of float = %u bytes\n", sizeof(float));
+printf("Size of char =  %u byte(s)\n", sizeof(char));
+printf("Size of int =  %u byte(s)\n", sizeof(int));
+printf("Size of long int = %u byte(s)\n", sizeof(long int));
+printf("Size of long long int = %u byte(s)\n", sizeof(long long int));
+printf("Size of float = %u byte(s)\n", sizeof(float));
 return (0);
 }

1f57bc445854a6e418c9ad7e79424553bfd3b6b7
kamisos3 <kamilasostre11@gmail.com>
2025-02-19 12:06:12 -0400

    Getting various types of sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,8 @@
 * Return:
 * Always 0
 */
-int main(void)   {
+int main(void)   
+{
 printf("Size of a char:  %u byte(s)\n", sizeof(char));
 printf("Size of an int:  %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long int));

6d2cfdf3092f7b484dcfdbf76ee173d1298fccf5
kamisos3 <kamilasostre11@gmail.com>
2025-02-19 11:44:26 -0400

    Getting various types of sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 printf("size of char =  % d\n", sizeof(char));
 printf("size of int =  % d\n", sizeof(int));
 printf("size of long int =  % d\n", sizeof(long int));
-printf("long int=%ldulong_max=%lu\n", LONG_MAX, ULONG_MAX);
+printf("long_max=%ld\nulong_max=%lu\n", LONG_MAX, ULONG_MAX);
 printf("size of float =  % d\n", sizeof(float));
 return (0);
 }

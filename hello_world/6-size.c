2c1b02bca05d75edb32d7257be67c12fe715901f
kamisos3 <kamilasostre11@gmail.com>
2025-02-19 11:11:13 -0400

    Getting various types of sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,12 +5,12 @@
 * Return:
 * Always 0
 */
-int main(void)
-{ 
-printf("sizeof(char)=%ul",sizeof(char));
-printf("sizeof(int)=%ul",sizeof(int));
-printf("sizeof(long int)=%ul",sizeof(long int));
-printf("sizeof(long long int)=%ul",sizeof(long long int));
-printf("sizeof(float)=%ul",sizeof(float));
+int main(void) 
+{
+printf("size of char =  % d\n", sizeof(char));
+printf("size of int =  % d\n", sizeof(int));
+printf("size of long int =  % d\n", sizeof(long int));
+printf("size of long long int =  % d\n", sizeof(long long int));
+printf("size of float =  % d\n", sizeof(float));
 return (0);
 }

8fb0438d76a10d37f52c301760f2a6f5afffe894
kamisos3 <kamilasostre11@gmail.com>
2025-02-20 10:28:51 -0400

    Declaring negative or positive value statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if( n > 0)
 {
 printf("is positive\n");
 }
-if(0)
+if (0)
 {
 printf("is zero\n");
 }
-if(n < 0)
+if (n < 0)
 {
 printf("is negative\n");
 }

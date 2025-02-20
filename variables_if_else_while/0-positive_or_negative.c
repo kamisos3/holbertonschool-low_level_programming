5264ea048851cacab1aa84ed3e64005339c64e1d
kamisos3 <kamilasostre11@gmail.com>
2025-02-20 10:51:01 -0400

    Declaring negative or positive value statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,17 +3,20 @@
 #include <time.h>
 /**
  * main-Declaring using arithmetic operations.
+ *
  * Return:
  * Always 0
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-	        printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	if (n == 0)
 	{

f61fe345374807568e2a4d34155a7870481b1f71
kamisos3 <kamilasostre11@gmail.com>
2025-02-23 18:01:21 -0400

    Reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Reverse alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letters = 122;
+
+	while (letters < 96)
+	{
+		putchar(letters);
+		letters--;
+	}
+	putchar('\n');
+	return (0);
+}

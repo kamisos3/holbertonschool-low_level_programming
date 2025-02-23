66d62eeb51ab404b7b1a839b8a81b9cffa56998b
kamisos3 <kamilasostre11@gmail.com>
2025-02-23 15:18:57 -0400

    Alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Excluding letters from printing
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letters = 97;
+
+
+	while (letters < 123)
+	{
+		if (letters != 101 && letters != 113)
+		{
+			putchar(letters);
+		}	
+		letters++;	
+	}
+	putchar('\n');
+	return (0);
+}	

f82902a8fb29bc7bffc7eda28de691644314b868
kamisos3 <kamilasostre11@gmail.com>
2025-02-24 12:17:09 -0400

    Verifying if c is a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isdigit-function that looks for digits
+ * _isdigit - looks if c is a digit
+ * @c: check character
  * Returns: 1 if c is a digit, 0 if not
- *
  */
 int _isdigit(int c)
 {
-	if (c >= 9)
+	if (c >= 0 && c <= 9)
 	{
 		return (1);
 	}

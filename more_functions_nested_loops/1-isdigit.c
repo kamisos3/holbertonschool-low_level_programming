34986ef089df036812d491a425aa2e673950762e
kamisos3 <kamilasostre11@gmail.com>
2025-02-24 12:23:30 -0400

    Verifying if c is a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,12 @@
 /**
  * _isdigit - looks if c is a digit
  * @c: check character
- * Returns: 1 if c is a digit, 0 if not
+ *
+ * Return: 1 if c is a digit, 0 if not
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}

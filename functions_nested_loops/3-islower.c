ba7a5e46835ac1aa024381d852b7ab42814108ef
kamisos3 <kamilasostre11@gmail.com>
2025-02-23 21:47:30 -0400

    Program that looks for lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - looks for lowecase characters
+ * @c: check character 
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

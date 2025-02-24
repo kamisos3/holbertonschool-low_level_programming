7efad23f6656787392b803bab0b9468a88ecb37c
kamisos3 <kamilasostre11@gmail.com>
2025-02-24 12:10:07 -0400

    Verifying if c is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit-function that looks for digits
+ * Returns: 1 if c is a digit, 0 if not
+ *
+ */
+int _isdigit(int c)
+{
+	if (c >= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

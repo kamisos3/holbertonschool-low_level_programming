f00140226c5d4374fd7574578057deb179e1c099
kamisos3 <kamilasostre11@gmail.com>
2025-02-23 22:54:54 -0400

    Add two intengers and result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add-Function that prints two intengers and returns
+ *
+ * @a:int val
+ * @b:int val
+ *
+ * Return:
+ * Sum of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

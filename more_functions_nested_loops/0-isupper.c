422a5b5f8d7fc606cc3ad15735823f54869867ee
kamisos3 <kamilasostre11@gmail.com>
2025-02-24 11:15:07 -0400

    Function that tells if c is uppercase.

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isupper-checks if a character is upperccase
+ * _isupper - checks if a character is uppercase
  * @c: check character
  * Return: 1 if c is uppercase, 0 if is not
  */
-int _isuppe(int c)
+int _isupper(int c)
 {
 	if (c >= 67 && c <= 99)
 	{

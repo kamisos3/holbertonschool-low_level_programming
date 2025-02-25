e56393b40ffc0955e67bbe0c59252030b026aa9d
kamisos3 <kamilasostre11@gmail.com>
2025-02-25 15:49:46 -0400

    Printing 0 to 14 multiple lines

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b > 9)
 			{
@@ -20,7 +20,7 @@ void more_numbers(void)
 			}
 			else
 				_putchar(b + '0');
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }

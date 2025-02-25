4012336d77d318b0652ec296df2c43e9be30bb8a
kamisos3 <kamilasostre11@gmail.com>
2025-02-25 14:43:22 -0400

    Printing from 0 to 14 for 10 lines

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print more numbers
+ *
+ */
+void print_most_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}

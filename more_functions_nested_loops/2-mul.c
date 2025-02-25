f16cf40c628747dd5eefe18c1a2dd8a1859b1f05
kamisos3 <kamilasostre11@gmail.com>
2025-02-25 10:43:26 -0400

    Multiplying two intengers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - character to multiply two intengers
+ * @a: first intenger
+ * @b: second intenger
+ * Return:
+ * Always 0
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}

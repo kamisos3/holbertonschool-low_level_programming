242d9b84c61d978190246f1f3c22c9ec0d9f26e6
kamisos3 <kamilasostre11@gmail.com>
2025-02-26 12:35:00 -0400

    Printing line diagonally

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints characters diagonally
+ * @n: check character
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (n = 0; n <= 0; n++)
+		{
+			if (i == n)
+				_putchar('\\');
+
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

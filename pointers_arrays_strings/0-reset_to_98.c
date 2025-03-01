9fb4605ba2452df4fdad7a6ca153300f27e3719f
kamisos3 <kamilasostre11@gmail.com>
2025-03-01 10:52:27 -0400

    Replacing intenger value

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,22 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * reset_to_98 - Dereferencing pointer value to 98
- * Return: 0
+ * @n: check character
  */
 void reset_to_98(int *n)
 {
-	*n = 0;
-
-	printf ("Value of 'n': %d\n", *n);
-	printf("Address of 'n': %p\n", (void *)n);
+	*n = 98;
 }
 
-int main(void)
-{
-	int num = 0;
-	printf("Original value: %d\n", num);
-	reset_to_98(&num);
-	printf("After function call: %d\n", num);
-	return (0);
-}

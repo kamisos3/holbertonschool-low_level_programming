8d53032259dc16cddb942a79d171580f7f666257
kamisos3 <kamilasostre11@gmail.com>
2025-03-01 18:49:26 -0400

    Puts half string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,28 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  * puts_half - Printing half the characters
  * @str: check characters
  */
 void puts_half(char *str)
 {
-	str[5] = '\0';
-	_putchar(*str);
+	int i = _strlen(str);
+	int j;
+
+	if (i % 2 == 0)
 	{
-		_putchar('\n');
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
 	}
+	else
+	{
+		for (j = (i + 1) / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
+

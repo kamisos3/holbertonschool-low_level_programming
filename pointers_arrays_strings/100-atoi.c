c1cec55c6615706227162f55d7a2b89e1c12a66a
kamisos3 <kamilasostre11@gmail.com>
2025-03-02 14:00:13 -0400

    String to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,18 @@
 #include "main.h"
 /**
- * _atoi - Convert string to an integer
- * @s: check characters
- * Return: Integer with sign
+ * _atoi - String to integer
+ * @s:verify characters
+ *
+ * Return: String value
  */
 int _atoi(char *s)
 {
-	int num = 0;
 	int i = 0;
 	int sign = 1;
-	int found_number = 0;
+	int num = 0;
+	int digit;
 
-
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-
-	while (s[i] == '-' || s[i] == '+')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -26,14 +21,20 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		num = num  * 10 + (s[i] - '0');
-		found_number = 1;
+		digit = s[i] - '0';  /* Converts number to integer */
+
+		if (num > 214748364 || (num == 214748364 && digit > 7))
+		{
+			if (sign == 1)
+				return (2147483647); /* Maximum + value */
+			else
+				return (-2147483648); /* Maximum - value */
+		}
+
+		num = num * 10 + digit;
 		i++;
-	}
-	if (!found_number)
-	{
-		return (0);
 	}
 
 	return (num * sign);
 }
+

8ce56007fd93e7ece724c1cb8e4b18949d816038
kamisos3 <kamilasostre11@gmail.com>
2025-03-01 20:24:38 -0400

    Print array elements

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,12 @@ void print_array(int *a, int n)
 	{
 		if (i == 0)
 		{
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		}
 		else
 		{
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		}
 	}
+	printf("\n");
 }

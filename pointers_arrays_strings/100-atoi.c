bacddf1f611ca9c354bc752361fa2921e447407e
kamisos3 <kamilasostre11@gmail.com>
2025-03-02 12:20:18 -0400

    String to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,30 @@ int _atoi(char *s)
 	int num = 0;
 	int i = 0;
 	int sign = 1;
+	int found_number = 0;
 
 
-	while (s[i] != '+' || s[i] == '-')
+	while (s[i] == ' ')
 	{
 		i++;
 	}
 
-	if (s[i] == '-' || s[i] == '+')
+	while (s[i] == '-' || s[i] == '+')
 	{
-		sign = (s[i] == '-') ? -1 : 1;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		num = num  * 10 + (s[i] - '0');
+		found_number = 1;
 		i++;
+	}
+	if (!found_number)
+	{
+		return (0);
 	}
 
 	return (num * sign);

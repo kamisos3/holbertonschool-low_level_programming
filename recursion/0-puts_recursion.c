63e68bbff602a919f020f15856bf4ea520c6bb88
kamisos3 <kamilasostre11@gmail.com>
2025-03-08 12:53:23 -0400

    Printing string with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s:string to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+Using recursion to breakdown big problems into smaller tasks to fix while coding and learning when not to use it.

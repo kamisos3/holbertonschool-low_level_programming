b9d5163e511a79629e871e28acf25c538a2d0b79
kamisos3 <kamilasostre11@gmail.com>
2025-03-09 09:18:46 -0400

    Raising x by the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x:base number
+ * @y:exponent
+ *
+ * Return: The result of x raised to the power of y, or -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}

1666728292e4f99e291bfcbebf944d12a224c637
kamisos3 <kamilasostre11@gmail.com>
2025-03-22 17:30:24 -0400

    Searches for integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -20,8 +20,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) == 1)/*Searches for match*/
-			return (1);/*Returns index of the first element*/
+		if (cmp(array[i]) != 0)/*Searches for match*/
+			return (i);/*Returns index of the first element*/
 	}
 	return (-1); /*No match was found*/
 }

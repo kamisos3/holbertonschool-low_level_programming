b530c919e55ac29a40cbeec1bae74853b1c27465
kamisos3 <kamilasostre11@gmail.com>
2025-03-23 13:46:10 -0400

    Sums of all it's parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
 	int sum = 0;
+	unsigned int i;
 	va_list args;
 
 	if (n == 0)
@@ -25,5 +25,6 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(args, int);
 	}
 	va_end(args);
+
 	return (sum);
 }

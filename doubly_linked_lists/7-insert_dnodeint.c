54f26f472164c7e8de0c6335922786691c4e257a
kamisos3 <kamilasostre11@gmail.com>
2025-04-06 21:02:30 -0400

    Fixing output

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->next = temp->next;
-	new_node->next = temp;
+	new_node->prev = temp;
 
 	if (temp->next)
 		temp->next->prev = new_node;

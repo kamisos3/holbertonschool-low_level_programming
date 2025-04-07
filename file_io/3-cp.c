c9339f79b1b2ce3660888b78cf747b045fe89526
kamisos3 <kamilasostre11@gmail.com>
2025-04-07 17:26:07 -0400

    Fixing compilation of function that copies a file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include "main.h"
+
+#define BUFFER_SIZE 1024
 /**
  * main - Appends file
  * @argc: argument count
@@ -7,50 +10,49 @@
  *
  * Return: (0)
  */
-int main(int arcg, char *argv[])
-{ 
-	int file_from, file_to, read_count, close_status;
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, rd_bytes, wr_bytes;
 	char buffer[BUFFER_SIZE];
 
-	if (argv != 3) {
+	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1) {
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1) {
-		close(file_from);
-		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
 	}
-
-	while ((read_count = read(file_from, buffer, BUFFER_SIZE)) > 0) {
-		if (write(file_to, buffer, read_count) != read_count) {
-			close(file_from);
-			close(file_to);
-			print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
+	while ((rd_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		wr_bytes = write(fd_to, buffer, rd_bytes);
+		if (wr_bytes != rd_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
 		}
 	}
-
-	if (read_count == -1) {
-		close(file_from);
-		close(file_to);
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	if (rd_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
-
-	close_status = close(file_from);
-	if (close_status == -1) {
-		print_error_and_exit(100, "Error: Can't close fd %d\n", file_from);
-	}
-
-	close_status = close(file_to);
-	if (close_status == -1) {
-		print_error_and_exit(100, "Error: Can't close fd %d\n", file_to);
-	}
+	close(fd_from);
+	close(fd_to);
 
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -21,5 +21,6 @@ void print_error_and_exit(int code, const char *message, const char *filename)
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 
 #endif

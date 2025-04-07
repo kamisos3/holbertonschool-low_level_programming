e1754b4a8780b972b65ceb879f14a01e2f1c07fb
kamisos3 <kamilasostre11@gmail.com>
2025-04-07 18:42:34 -0400

    Fixing copy file function adding a handler file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-
-#define BUFFER_SIZE 1024
 /**
  * main - Appends file
  * @argc: argument count
@@ -12,47 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd_bytes, wr_bytes;
-	char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
+	{
+		print_error_and_exit(99, "Error: Can't write to %s\n", NULL);
+	}
+	fd_from = open_file(argv[1], O_RDONLY, 0);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
-	while ((rd_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		wr_bytes = write(fd_to, buffer, rd_bytes);
-		if (wr_bytes != rd_bytes)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-	if (rd_bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-	close(fd_from);
-	close(fd_to);
 
+	copy_content(fd_from, fd_to);
+
+	if (close(fd_from) == -1)
+	{
+		print_error_and_exit(100, "Error: Can't close fd %d\n", NULL);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		print_error_and_exit(100, "Error: Can't cloose fd %d\n", NULL);
+
+	}
 	return (0);
 }
---
+++ file_io/3-cp_handler.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * print_error_and_exit - Prints error message
+ * @code: exit code
+ * @message: error message
+ * @filename: file name that has errror
+ *
+ *
+ */
+void print_error_and_exit(int code, const char *message, const char *filename)
+{
+	dprintf(STDERR_FILENO, message, filename);
+	exit(code);
+}
+
+/**
+* open_file - Opens a file with certain flags/permissions
+* @filename: file name
+* @flags: flags for open
+* @permissions: Allows to open
+*
+* Return: File descriptor, -1 if fails
+*/
+int open_file(const char *filename, int flags, int permissions)
+{
+	return (open(filename, flags, permissions));
+}
+
+/**
+* copy_content - Copies content of file
+* @fd_from: file descriptor, source file
+* @fd_to: file descriptor, destination file
+*
+* Return: Copied content
+*/
+void copy_content(int fd_from, int fd_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t rd_bytes, wr_bytes;
+
+	while ((rd_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		wr_bytes  = write(fd_to, buffer, rd_bytes);
+		if (wr_bytes != rd_bytes)
+		{
+			print_error_and_exit(99, "Error: Can't write to file\n", NULL);
+		}
+		if (rd_bytes == -1)
+		{
+			print_error_and_exit(98, "Error: Can't read from filr\n", NULL);
+		}
+	}
+}
